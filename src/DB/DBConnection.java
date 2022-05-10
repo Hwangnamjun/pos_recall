f8290c5fe73423762d0eec3b33e8bb5300043caf
Hwangnamjun <dudrn0585@naver.com>
2022-05-10 16:20:06 +0900

    #1 DBconnection 변경
    기존 local DB -> 개인 Oracle Cloud DB로 변경

--- src/DB/DBConnection.java
+++ src/DB/DBConnection.java
@@ -19,12 +19,11 @@ public class DBConnection {
 		Connection conn = null;
 		try 
 		{
-			String user = "ddris";
-			String password = "ddris";
-			String url = "jdbc:oracle:thin:@10.10.10.11:1521:DDSDB";
+			String user = "ADMIN";
+			String password = "mihwum-gojby3-hetgYf";
+			String url = "jdbc:oracle:thin:@hwangnamjun_medium?TNS_ADMIN=/Users/hwangnamjun/Oracle_backup/Wallet_Hwangnamjun/";
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(url, user, password);
-			System.out.println("연결완료");
 			
 		}catch(ClassNotFoundException e){
 			System.out.println("db드라이버 로딩 실패" + e.toString());
@@ -33,7 +32,7 @@ public class DBConnection {
 		}catch(Exception e) {
 			System.out.println("에러" + e.toString());
 		}
-		
+
 		return conn;
 	}
 	
